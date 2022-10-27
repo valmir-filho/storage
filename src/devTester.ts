3bdcb78d5f6e2a985ee13d211e0b681184767fa7
Valmir <vmcfilho@gmail.com>
2022-10-27 11:26:08 -0300

    Delete devTester.ts

--- src/devTester.ts
+++
@@ -1,9 +0,0 @@
-import * as storage from "./fileManager";
-
-const archive = new storage.Archive("teste.txt");
-
-// archive.createNewFile();
-// archive.appendFile();
-// archive.deleteFile();
-// archive.copyFile();
-// archive.renameFile("teste2.txt");
